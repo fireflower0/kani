f4a5e718918532ccc5ba95f5638c3fe2c15646f5
fireflower0 <fireflower0@hotmail.co.jp>
2019-09-13 17:32:06 +0900

    Fixed a skeleton

--- skeleton/project/app.lisp
+++ skeleton/project/app.lisp
@@ -1,18 +1,17 @@
 (uiop:define-package #:<% @var name %>/app
-  (:use #:cl
-        #:<% @var name %>/gtk3-tools/widget
-        #:<% @var name %>/gtk3-tools/event)
+  (:use #:cl)
   (:export #:main))
 (in-package #:<% @var name %>/app)
 
 (defun main (&rest args)
-  (let (;; Create widget
-        (window (create-window :type :toplevel
-                               :title "Hello, world!"
-                               :default-width 640
-                               :default-height 480)))
-    (gtk:within-main-loop
-     ;; Event
-     (destroy-window window)
-     ;; Show widget
+  (gtk:within-main-loop
+   (let ((window (make-instance 'gtk:gtk-window
+                                :type  :toplevel
+                                :title "Hello, world!"
+                                :default-width  640
+                                :default-height 480)))
+     (gobject:g-signal-connect window "destroy"
+                               (lambda (widget)
+                                 (declare (ignore widget))
+                                 (gtk:leave-gtk-main)))
      (gtk:gtk-widget-show-all window))))
