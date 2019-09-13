ada20481f892ffbddd3a7171da9b873dc9080fd0
fireflower0 <fireflower0@hotmail.co.jp>
2019-09-13 09:14:14 +0900

    Fixed a gtk-tools widget in skeleton

--- skeleton/project/gtk3-tools/widget.lisp
+++ skeleton/project/gtk3-tools/widget.lisp
@@ -5,77 +5,75 @@
 (in-package #:<% @var name %>/gtk3-tools/widget)
 
 (defun create-window (&key
-                        type
-                        ;; accept-focus
-                        ;; application
-                        ;; attached-to
-                        ;; decorated
-                        default-height
-                        default-width
-                        ;; deletable
-                        ;; destroy-with-parent
-                        ;; focus-on-map
-                        ;; focus-visible
-                        ;; gravity
-                        ;; has-resize-grip
-                        ;; has-toplevel-focus
-                        ;; hide-titlebar-when-maximized
-                        ;; icon
-                        ;; icon-name
-                        ;; is-active
-                        ;; is-maximized
-                        ;; mnemonics-visible
-                        ;; modal
-                        ;; opacity
-                        ;; resizable
-                        ;; resize-grip-visible
-                        ;; role
-                        ;; screen
-                        ;; skip-pager-hint
-                        ;; skip-taskbar-hint
-                        ;; startup-id
-                        title
-                        ;; transient-for
-                        ;; type-hint
-                        ;; urgency-hint
-                        ;; window-position
-                        )
+                        (type :toplevel)
+                        (accept-focus t)
+                        (application nil)
+                        (attached-to nil)
+                        (decorated t)
+                        (default-height -1)
+                        (default-width -1)
+                        (deletable t)
+                        (destroy-with-parent nil)
+                        (focus-on-map t)
+                        (focus-visible t)
+                        (gravity :north-west)
+                        (has-resize-grip nil)
+                        (has-toplevel-focus nil)
+                        (hide-titlebar-when-maximized nil)
+                        (icon nil)
+                        (icon-name "")
+                        (is-active nil)
+                        (is-maximized nil)
+                        (mnemonics-visible t)
+                        (modal nil)
+                        (opacity 1)
+                        (resizable t)
+                        (resize-grip-visible nil)
+                        (role "")
+                        (screen nil)
+                        (skip-pager-hint nil)
+                        (skip-taskbar-hint nil)
+                        (startup-id "")
+                        (title "")
+                        (transient-for nil)
+                        (type-hint :normal)
+                        (urgency-hint nil)
+                        (window-position :none))
   (make-instance 'gtk:gtk-window
                  :type type
-                 ;; :accept-focus accept-focus
-                 ;; :application application
-                 ;; :attached-to attached-to
-                 ;; :decorated decorated
+                 :accept-focus accept-focus
+                 :application application
+                 :attached-to attached-to
+                 :decorated decorated
                  :default-height default-height
                  :default-width default-width
-                 ;; :deletable deletable
-                 ;; :destroy-with-parent destroy-with-parent
-                 ;; :focus-on-map focus-on-map
-                 ;; :focus-visible focus-visible
-                 ;; :gravity gravity
-                 ;; :has-resize-grip has-resize-grip
-                 ;; :has-toplevel-focus has-toplevel-focus
-                 ;; :hide-titlebar-when-maximized hide-titlebar-when-maximized
-                 ;; :icon icon
-                 ;; :icon-name icon-name
-                 ;; :is-active is-active
-                 ;; :is-maximized is-maximized
-                 ;; :mnemonics-visible mnemonics-visible
-                 ;; :modal modal
-                 ;; :opacity opacity
-                 ;; :resizable resizable
-                 ;; :resize-grip-visible resize-grip-visible
-                 ;; :role role
-                 ;; :screen screen
-                 ;; :skip-pager-hint skip-pager-hint
-                 ;; :skip-taskbar-hint skip-taskbar-hint
-                 ;; :startup-id startup-id
+                 :deletable deletable
+                 :destroy-with-parent destroy-with-parent
+                 :focus-on-map focus-on-map
+                 :focus-visible focus-visible
+                 :gravity gravity
+                 :has-resize-grip has-resize-grip
+                 :has-toplevel-focus has-toplevel-focus
+                 :hide-titlebar-when-maximized hide-titlebar-when-maximized
+                 :icon icon
+                 :icon-name icon-name
+                 :is-active is-active
+                 :is-maximized is-maximized
+                 :mnemonics-visible mnemonics-visible
+                 :modal modal
+                 :opacity opacity
+                 :resizable resizable
+                 :resize-grip-visible resize-grip-visible
+                 :role role
+                 :screen screen
+                 :skip-pager-hint skip-pager-hint
+                 :skip-taskbar-hint skip-taskbar-hint
+                 :startup-id startup-id
                  :title title
-                 ;; :transient-for transient-for
-                 ;; :type-hint type-hint
-                 ;; :urgency-hint urgency-hint
-                 ;; :window-position window-position
-                 ))
+                 :transient-for transient-for
+                 :type-hint type-hint
+                 :urgency-hint urgency-hint
+                 :window-position window-position))
 
 (defun create-button (&key
                         ;; always-show-image
