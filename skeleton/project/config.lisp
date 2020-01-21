a54a59c84cffc68d481dc4c9f05f27f2c2e004fc
fireflower0 <fireflower0@hotmail.co.jp>
2020-01-22 00:31:06 +0900

    Fixed a skeleton

--- skeleton/project/config.lisp
+++ skeleton/project/config.lisp
@@ -1,12 +1,21 @@
 (uiop:define-package #:<% @var name %>/config
-  (:use #:cl
-        #:envy))
+  (:use #:cl)
+  (:import-from :envy
+                :config-env-var
+                :defconfig)
+  (:export :config))
 (in-package #:<% @var name %>/config)
 
 (setf (config-env-var) "APP_ENV")
 
+;;;
+;; File & Directory Path
+
 (defparameter *app-root*
   (asdf:system-source-directory :<% @var name %>))
+
+;;;
+;; DB Config
 
 (defparameter *db-name*
   (merge-pathnames
@@ -15,3 +24,9 @@
 
 (defconfig :common
     `(:databases ((:maindb :sqlite3 :database-name ,*db-name*))))
+
+;;;
+;; Config Function
+
+(defun config (&optional key)
+  (envy:config #.(package-name *package*) key))
